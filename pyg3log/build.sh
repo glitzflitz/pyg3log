308685a5d7fceae06c0e8a31f05666fab747b3cc
GreyDireWolf <greydirewolf@wolf.com>
2019-09-30 23:01:59 +0530

    Cleanup

--- pyg3log/build.sh
+++
@@ -1,1 +0,0 @@
-g++ -O3 -Wall -shared -std=c++17 -fPIC -lg3logger -lg3logrotate -I/usr/include/python3.7m -I/home/direwolf/.local/include/python3.7m pyg3log.cpp -o pyg3log.cpython-37m-x86_64-linux-gnu.so
